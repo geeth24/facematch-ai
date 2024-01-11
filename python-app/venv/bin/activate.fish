d4279a6d56dd6b96dcdd925a946ae730c016d384
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-01-10 23:38:07 -0600

    Update virtual environment paths

--- python-app/venv/bin/activate.fish
+++ python-app/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/geeth/Development/revela-api/venv"
+set -gx VIRTUAL_ENV "/Users/geeth/Development/facematch-ai/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
